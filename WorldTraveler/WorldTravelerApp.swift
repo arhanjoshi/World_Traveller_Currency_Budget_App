7aa45bf8900d2ccd5b3229db61b0e7e4c9dce21c
arhanjoshi <ajoshi92@asu.edu>
2025-09-15 12:55:23 -0700

    Initial Commit

---
+++ WorldTraveler/WorldTravelerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldTravelerApp.swift
+//  WorldTraveler
+//
+//  Created by Arhan Joshi on 9/15/25.
+//
+
+import SwiftUI
+
+@main
+struct WorldTravelerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WorldTravelerTests/WorldTravelerTests.swift
@@ -0,0 +1,17 @@
+//
+//  WorldTravelerTests.swift
+//  WorldTravelerTests
+//
+//  Created by Arhan Joshi on 9/15/25.
+//
+
+import Testing
+@testable import WorldTraveler
+
+struct WorldTravelerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
